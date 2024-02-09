b83d58c7561d70c5707bfbd88f1066aff09744e9
suntrise <89229642+suntrise@users.noreply.github.com>
2024-02-09 12:13:08 +0800

    Add files via upload

---
+++ i18n/zh_CN.py
@@ -0,0 +1,67 @@
+langInfo = {
+    "engName": "Chinese (Simplified)",
+    "orgiName": "简体中文"
+}
+
+infomation = {
+    "wslWarning": "发现您正在使用 WSL, 实际上, 我们更推荐您直接使用 Windows 版本以避免潜在的 BUG",
+    "cygwinWarning": "发现您正在使用 Cygwin, 实际上, 我们更推荐您直接使用 Windows 版本以避免潜在的 BUG",
+    "nonTestWarning": "除 Windows 外, 该软件没有做过稳定性测试, 可能会存在一些开发者也不清楚的 BUG 需要修复"
+}
+
+menuBar = {
+    "files": "文件",
+    "openFile": "打开",
+    "getFromNeteaseMusic": "从网易云中获取",
+    "exit": "退出",
+    "media": "媒体",
+    "channels": "左右声道",
+    "balance": "平衡",
+    "shiftLeft": "向左移",
+    "shiftRight": "向右移",
+    "position": "进度",
+    "forward10s": "快进 10s",
+    "back10s": "后退 10s",
+    "times": "倍速",
+    "0.5x": "0.5x",
+    "1x": "1x",
+    "1.5x": "1.5x",
+    "2x": "2x",
+    "volume": "音量",
+    "audioInfo": "媒体信息",
+    "help": "帮助",
+    "about": "关于"
+}
+
+mainMenu = {
+    "none": "无",
+    "unknownMusic": "未知歌曲",
+    "unknownArtist": "未知作曲家",
+    "unknownAlbum": "未知专辑",
+    "moreInfo": "详细信息",
+    "songLoaded": "已加载歌曲:",
+    "about": "关于",
+    "beenTop": "已置顶",
+    "beenUntop": "已取消置顶",
+    "resetMenuBar": "按下 Ctrl+H 复原顶栏!",
+    "enableLoop": "已启用循环播放",
+    "disableLoop": "已禁用循环播放"
+}
+
+tooltips = {
+    "alwaysOnTop": "置顶",
+    "cancelAlwaysOnTop": "取消置顶",
+    "hideMenuBar": "隐藏顶栏 (Ctrl+H)",
+    "audioPosition": "歌曲进度",
+    "playOrPause": "播放/暂停",
+    "playInLoop": "循环播放",
+    "volume": "音量",
+    "songList": "歌单",
+    "audioInfo": "媒体信息",
+    "settings": "设置"
+}
+
+songList = {
+    "songList": "歌单",
+    "construction": "施工中"
+}
